1abf9b396d0e990d9890155a3c0cc3c4525a4c63
dfgion <113937888+dfgion@users.noreply.github.com>
2022-10-11 22:58:12 +0300

    Pep8

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 def main(name):
-    files=['1.txt', '2.txt', '3.txt']
-    inf_f={}
+    files = ['1.txt', '2.txt', '3.txt']
+    inf_f = {}
     for file in files:
-        with open(file, encoding='utf-8') as f:
-            supp_inf_f={file : [len(f.readlines())]}
+        with open(file, encoding = 'utf-8') as f:
+            supp_inf_f = {file : [len(f.readlines())]}
             f.seek(0)
             supp_inf_f[file].append(f.readlines())
             inf_f.update(supp_inf_f)
-    sorted_dict=dict(sorted(inf_f.items(), key=lambda x: x[1][1], reverse=True))
-    with open(name, 'w', encoding='utf-8') as wr_f:
+    sorted_dict = dict(sorted(inf_f.items(), key = lambda x: x[1][1], reverse = True))
+    with open(name, 'w', encoding = 'utf-8') as wr_f:
         for key in sorted_dict.keys():
             wr_f.write(key)
             wr_f.write('\n')
